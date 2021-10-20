fae4e7c5e3ee8eb202cd5466b08f9032816390a1
Kunal Bhandari <kunalbhandari101@gmail.com>
2021-10-20 14:11:43 +0530

    H4X0R News' skjnddsf :q! :q

---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Kunal Bhandari on 12/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .navigationBarTitle("H4X0R News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
